b17a7cae63080d56fb50cc8acaaa639ed834cec6
Niket kumar <niketkumar18@gmail.com>
2021-02-09 17:49:50 +0530

    Spring boot test with Mockito

---
+++ Spring-Boot-test/src/main/java/com/nik/core/services/HelloService.java
@@ -0,0 +1,7 @@
+package com.nik.core.services;
+
+public interface HelloService {
+
+    String get();
+
+}
---
+++ Spring-Boot-test/src/main/java/com/nik/core/services/HelloServiceImpl.java
@@ -0,0 +1,18 @@
+package com.nik.core.services;
+
+import com.nik.core.repository.HelloRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloServiceImpl implements HelloService {
+
+    @Autowired
+    HelloRepository helloRepository;
+
+    @Override
+    public String get() {
+        return helloRepository.get();
+    }
+
+}
